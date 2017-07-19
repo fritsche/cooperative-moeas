de7aa55d1cad6f07c7d52f9ecdc24973b8ec3f50
Gian Mauricio Fritsche <gian.fritsche@gmail.com>
2017-07-19 15:57:50 -0300

    Update ParameterizedRunner.java
    Set log level to warning

--- jmetal-exec/src/main/java/org/uma/jmetal/runner/multiobjective/ParameterizedRunner.java
+++ jmetal-exec/src/main/java/org/uma/jmetal/runner/multiobjective/ParameterizedRunner.java
@@ -250,9 +250,9 @@ public class ParameterizedRunner<Result> extends ExecuteAlgorithms<Solution<?>, 
     public static void main(String[] args) {
         
         // do not print info
-//        JMetalLogger.logger.setLevel(Level.WARNING);
+        JMetalLogger.logger.setLevel(Level.WARNING);
         
-        JMetalLogger.logger.setLevel(Level.ALL);
+        // JMetalLogger.logger.setLevel(Level.ALL);
                               
         if (args.length != 5) {
             throw new JMetalException("Needed arguments: "
